1522d2d1d5e21145ebd6d7f312edafdb96089679
pochonm04 <pochonm04@WSTEMFB32-05.stu.net.fr.ch>
2023-03-28 09:27:40 +0200

    Javadoc wrk

--- src/ch/emf/atelierpo/wrk/Worker.java
+++ src/ch/emf/atelierpo/wrk/Worker.java
@@ -7,7 +7,12 @@ package ch.emf.atelierpo.wrk;
  */
 public class Worker {
 
-
+/** 
+ * Cette méthode permet un controle de numero de téléphone mobile  <b>et retourne VRAI si le numéro est controlé</b>
+ * @param prefix le prefix du pays
+ * @param numero le numerot du téléphone a controler
+ * @return VRAI si le controle à été efféctué
+ */
     public boolean controleNumeroTelMobile(String prefix, String numero) {
         return false;
     }
